a86a4642d86cecde6ff51889c92ced14b74123fd
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-21 19:46:00 +0100

    Designing the Application Project.

---
+++ Customers.Docs.Application/Contracts/Persistence/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Customers.Docs.Domain.Entities;
+
+namespace Customers.Docs.Application.Contracts.Persistence
+{
+    public interface ICustomerRepository : IAsyncRepository<Customer>
+    {
+    }
+}
