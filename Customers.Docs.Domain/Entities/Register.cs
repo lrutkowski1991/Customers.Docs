ff3ca7475ae25883023ab8d4a357174317029fd4
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-21 19:26:22 +0100

    Create the Domain Project.

---
+++ Customers.Docs.Domain/Entities/Register.cs
@@ -0,0 +1,11 @@
+﻿namespace Customers.Docs.Domain.Entities
+{
+    public class Register
+    {
+        public Guid RegisterId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsClosed { get; set; }
+    }
+}
