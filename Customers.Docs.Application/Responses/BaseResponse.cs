7ba2bb55d318ae95cb346c3beb23cf6d8e3150b9
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-28 19:13:20 +0100

    BaseResponse has been added.

---
+++ Customers.Docs.Application/Responses/BaseResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Customers.Docs.Application.Responses
+{
+    public class BaseResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public BaseResponse()
+        {
+            Success = true;
+        }
+        public BaseResponse(string message = null)
+        {
+            Success = true;
+            Message = message;
+        }
+        public BaseResponse(bool succes, string message = null)
+        {
+            Success = succes;
+            Message = message;
+        }
+    }
+}
